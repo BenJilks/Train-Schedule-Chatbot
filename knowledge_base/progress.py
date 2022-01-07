59dedb7cc24229568b2d9d6af14bd2ec7b39e3a2
BenJilks <benjyjilks@gmail.com>
2022-01-07 16:09:06 +0000

    Don't ignore progress reports, just queue them for later

--- knowledge_base/progress.py
+++ knowledge_base/progress.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from queue import Queue
 from threading import Lock
 import sys
 
@@ -14,11 +15,13 @@ class Progress:
     _bars: dict[str, ProgressBar]
     _last_bar_count: int | None
     _mutex: Lock
+    _report_queue: Queue[tuple[str, int, int]]
 
     def __init__(self):
         self._bars = {}
         self._last_bar_count = None
         self._mutex = Lock()
+        self._report_queue = Queue()
 
     def _draw_bar(self, bar: ProgressBar):
         progress = int((float(bar.done) / float(bar.out_of)) * BAR_LENGTH)
@@ -29,6 +32,10 @@ class Progress:
         print(f'] { bar.done } / { bar.out_of }', end='')
 
     def _clean_draw(self):
+        if len(self._bars) == 0:
+            print()
+            return
+
         self._last_bar_count = len(self._bars)
         for name, bar in self._bars.items():
             display_name = name if len(name) < NAME_LEN else (name[:(NAME_LEN - 3)] + '...')
@@ -45,6 +52,13 @@ class Progress:
         print('\033[G', end='')
         sys.stdout.flush()
 
+    def _update_report(self, name: str, done: int, out_of: int):
+        if done >= out_of:
+            if name in self._bars:
+                del self._bars[name]
+        else:
+            self._bars[name] = ProgressBar(done + 1, out_of)
+
     def display(self):
         if len(self._bars) == self._last_bar_count:
             self._quick_update()
@@ -56,15 +70,17 @@ class Progress:
         return
 
     def report(self, name: str, done: int, out_of: int):
-        # If we don't get the lock, just ignore the report
+        # If we don't get the lock, queue it up for later
         if not self._mutex.acquire(blocking=False):
+            self._report_queue.put((name, done, out_of))
             return
+        
+        # Report anything in the queue first
+        while not self._report_queue.empty():
+            self._update_report(*self._report_queue.get())
 
-        if done >= out_of:
-            if name in self._bars:
-                del self._bars[name]
-        else:
-            self._bars[name] = ProgressBar(done + 1, out_of)
+        self._update_report(name, done, out_of)
+
         self.display()
         self._mutex.release()
 
